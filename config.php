a4ff1ce0ac4ad9d031c7eca8c9f2069d8ef2a6e9
Muhammad Risqi Darmawan <59747952+mrisqid@users.noreply.github.com>
2021-03-07 17:05:58 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+require_once './vendor/autoload.php';
+require_once "class-db.php";
+ 
+define('CLIENT_ID', '3gRjKZlSv2fTyQmIkdWw');
+define('CLIENT_SECRET', 'kVv3Kd6XlxEzFmZ6bHK9QNyG6xwZB13z');
+define('REDIRECT_URI', 'http://acca94bca7e7.ngrok.io/php-zoom/callback.php');
